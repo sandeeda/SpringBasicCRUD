0c87523bc15a651d9c3179349dc037dcf950e1f9
Sandeep Das <sdsts28@outlook.com>
2023-03-12 12:38:32 -0400

    Removed hibernate

--- src/main/java/crud/dao/AdminDao.java
+++ src/main/java/crud/dao/AdminDao.java
@@ -1,27 +1,42 @@
 package crud.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
-import javax.transaction.Transactional;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import crud.model.Admin;
-import crud.model.Student;
 
 @Component
 public class AdminDao {
 	
-	@Autowired
-	private HibernateTemplate hibernateTemplate;
-	
+	private JdbcTemplate jdbcTemplate;
 
+	@Autowired
+	public AdminDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 	//get all admins
 	public List<Admin> getAdmins(){
-		return this.hibernateTemplate.loadAll(Admin.class);
+		String sql = "SELECT * FROM admin";
+		return jdbcTemplate.query(sql, new AdminRowMapper());
 	}
 	
+	// RowMapper for Student class
+		class AdminRowMapper implements RowMapper<Admin> {
+			public Admin mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Admin admin = new Admin();
+				admin.setAdminId(rs.getInt("adminId"));
+				admin.setUsername(rs.getString("username"));
+				admin.setPassword(rs.getString("password"));
+				return admin;
+			}
+		}
 	
 }
